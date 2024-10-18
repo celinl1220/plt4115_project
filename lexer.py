622ca5845c54287a98c4c231ff4d5ea562286e15
liemma <lemma75@gmail.com>
2024-10-17 23:32:15 -0400

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -26,76 +26,72 @@ class LexicalError(Exception):
     pass
 
 def scan(input_program):
-    tokens = [] # list of tokens to output (token_type, token_value)
-    current_state = "START" # keeps track of most recent state
-    token_value = "" # token value to add to token list
+    tokens = []  # List of tokens to output (token_type, token_value)
+    current_state = "START"  # Keeps track of the most recent state
+    token_value = ""  # Token value to add to token list
 
     for char in input_program:
-        if current_state == "START": # if at START state (starting new token)
-            if is_letter(char): # if char is a letter
-                token_value += char # add char to token value
-                current_state = "ID_OR_KEYWORD" # update current state to ID_OR_KEYWORD
-            elif is_digit(char): # if char is a number
-                token_value += char # add char to token value
-                current_state = "NUMBER" # update current state to NUMBER
+        if current_state == "START":  # If at START state (starting new token)
+            if is_letter(char):  # If char is a letter
+                token_value += char  # Add char to token value
+                current_state = "ID_OR_KEYWORD"  # Update current state to ID_OR_KEYWORD
+            elif is_digit(char):  # If char is a number
+                token_value += char  # Add char to token value
+                current_state = "NUMBER"  # Update current state to NUMBER
             elif is_music_note_start(char):
                 token_value += char
                 current_state = "MUSIC_NOTE"
-            elif is_punctuator(char): # if char is a punctuator
-                tokens.append(("PN", char)) # append char to token list as PN
-            elif is_whitespace(char): # if char is whitespace
-                current_state = "WHITESPACE" # update current state to WHITESPACE (don't need to append to token list?)
+            elif is_punctuator(char):  # If char is a punctuator
+                tokens.append(("PN", char))  # Append char to token list as PN
+            elif is_whitespace(char):  # If char is whitespace
+                continue  # Skip whitespace and continue to the next character
             else:
                 raise LexicalError(f"Unrecognized character: {char}")
 
-        elif current_state == "ID_OR_KEYWORD": # if current state is ID_OR_KEYWORD
-            if is_letter(char) or is_digit(char): # if char is letter or number
-                token_value += char # add char to token value
-            else: # else (id or keyword value is followed by non-letter/number -- whitespace, punctuation, unknown symbol)
-                if is_keyword(token_value): # if token value is a keyword
-                    tokens.append(("KW", token_value)) # append token value to token list as KW
-                else: # if token value is not a keyword
-                    tokens.append(("ID", token_value)) # append token value to token list as ID
-                token_value = "" # reset token value and current state
+        elif current_state == "ID_OR_KEYWORD":  # If current state is ID_OR_KEYWORD
+            if is_letter(char) or is_digit(char):  # If char is letter or number
+                token_value += char  # Add char to token value
+            else:  # Else (id or keyword value is followed by non-letter/number -- whitespace, punctuation, unknown symbol)
+                if is_keyword(token_value):  # If token value is a keyword
+                    tokens.append(("KW", token_value))  # Append token value to token list as KW
+                else:  # If token value is not a keyword
+                    tokens.append(("ID", token_value))  # Append token value to token list as ID
+                token_value = ""  # Reset token value and current state
                 current_state = "START"
 
-        elif current_state == "NUMBER": # if current state is NUMBER
-            if is_digit(char): # if char is a number
-                token_value += char # add char to token value
-            else: # if char is not a number
-                tokens.append(("NM", token_value)) # append token value to token list as NM
-                token_value = "" # reset token value and current state
+        elif current_state == "NUMBER":  # If current state is NUMBER
+            if is_digit(char):  # If char is a number
+                token_value += char  # Add char to token value
+            else:  # If char is not a number
+                tokens.append(("NM", token_value))  # Append token value to token list as NM
+                token_value = ""  # Reset token value and current state
                 current_state = "START"
 
-        elif "MUSIC_NOTE" in current_state: # if current state has substring MUSIC_NOTE
-            if current_state == "MUSIC_NOTE" and is_music_note_accidental(char): # if only first letter of music note and char is an accidental
-                token_value += char # add char to token value
-                current_state = "MUSIC_NOTE_ACC" # update current state to MUSIC_NOTE_ACC (music note with accidental)
-            elif (current_state == "MUSIC_NOTE" or current_state == "MUSIC_NOTE_ACC") and is_digit(char): # if current state is either MUSIC_NOTE or MUSIC_NOTE_ACC and char is a number
-                token_value += char # add char to token value
+        elif "MUSIC_NOTE" in current_state:  # If current state has substring MUSIC_NOTE
+            if current_state == "MUSIC_NOTE" and is_music_note_accidental(char):  # If only first letter of music note and char is an accidental
+                token_value += char  # Add char to token value
+                current_state = "MUSIC_NOTE_ACC"  # Update current state to MUSIC_NOTE_ACC (music note with accidental)
+            elif (current_state == "MUSIC_NOTE" or current_state == "MUSIC_NOTE_ACC") and is_digit(char):  # If current state is either MUSIC_NOTE or MUSIC_NOTE_ACC and char is a number
+                token_value += char  # Add char to token value
                 current_state = "MUSIC_NOTE_OCT"
-            elif is_music_note_duration(char): # if char is a music note duration
-                token_value += char # add char to token value
-                tokens.append(("MN", token_value)) # append token value to token list as MN
-                token_value = "" # reset token value and current state
+            elif is_music_note_duration(char):  # If char is a music note duration
+                token_value += char  # Add char to token value
+                tokens.append(("MN", token_value))  # Append token value to token list as MN
+                token_value = ""  # Reset token value and current state
                 current_state = "START"
             else:
-                token_value += char
                 raise LexicalError(f"Invalid MusicNote: {token_value}")
-                
-        elif current_state == "WHITESPACE": # if current state is WHITESPACE
-            if not is_whitespace(char): # if char is not white space
-                current_state = "START" # reset current state
 
-    if token_value: # if input is fully scanned and there is remaining token value
-        if current_state == "ID_OR_KEYWORD": # if current state is ID_OR_KEYWORD
-            if is_keyword(token_value): # if token value is a keyword
-                tokens.append(("KW", token_value)) # append token value to token list as KW
-            else: # if token value is not a keyword
-                tokens.append(("ID", token_value)) # append token value to token list as ID
-        elif current_state == "NUMBER": # if current state is NUMBER
-            tokens.append(("NM", token_value)) # append token value to token list as NM
-        elif current_state == "MUSIC_NOTE": # if current state is MUSIC_NOTE
-            tokens.append(("MN", token_value)) # append token value to token list as MN 
+    # After loop ends, process any remaining token value
+    if token_value:
+        if current_state == "ID_OR_KEYWORD":
+            if is_keyword(token_value):
+                tokens.append(("KW", token_value))
+            else:
+                tokens.append(("ID", token_value))
+        elif current_state == "NUMBER":
+            tokens.append(("NM", token_value))
+        elif current_state == "MUSIC_NOTE":
+            tokens.append(("MN", token_value))
 
     return tokens
